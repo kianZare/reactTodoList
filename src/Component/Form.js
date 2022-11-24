8ee34907dd74fc4bec03117aa0329b8c490707e4
Kian Zareinajad <kzareinajad@gmail.com>
2022-11-24 15:28:44 +0000

    reactTodoList v1

---
+++ src/Component/Form.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const form = ({setInputText, todos, setTodos, inputText}) =>{
+    const inputTextHandler = (e) => {
+        setInputText(e.target.value);
+    };
+    const submitTodoHandler = (e) =>{
+        e.preventDefault();
+        setTodos([
+            ...todos,
+            {text: inputText, completed: false, id: Date.now()}
+        ]);
+        setInputText("");
+    }
+    return(
+        <form>
+            <input value={inputText} onChange={inputTextHandler} type='text' className="todo-input"/>
+            <button onClick={submitTodoHandler} className="todo-button" type="submit">
+                <i className="fa-solid fa-square-plus"></i>
+            </button>
+            <div className="select">
+                <select name="todos" className="filter-todo">
+                    <option value="all">All</option>
+                    <option value="completed">Completed</option>
+                    <option value="uncompleted">Uncompleted</option>
+                </select>
+            </div>
+        </form>
+    );
+};
+
+export default form;
